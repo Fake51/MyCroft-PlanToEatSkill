7dee7902e35aa1c62493eb02ce6511d1dc8b9328
Peter Lind <peli@boozt.com>
2021-04-30 22:54:04 +0200

    adding category suggestion lookup improving code

--- __init__.py
+++ __init__.py
@@ -6,7 +6,9 @@ from mycroft.util.log import getLogger
 
 userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
 
-submitString = 'shopping_list_id={0}&autoStore=1&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D={1}&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0'
+submitString = 'shopping_list_id={0}&autoStore=1&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D={1}&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D={2}&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0&ingredients%5B%5D%5Bamount%5D=&ingredients%5B%5D%5Bunit%5D=&ingredients%5B%5D%5Btitle%5D=&ingredients%5B%5D%5Bnote%5D=&ingredients%5B%5D%5Bcategory%5D=&ingredients%5B%5D%5Bstore%5D=0'
+
+baseUrl = "https://www.plantoeat.com/{0}"
 
 LOGGER = getLogger(__name__)
 
@@ -47,17 +49,24 @@ class PlanToEat(MycroftSkill):
             return
 
         response = self.session.get(
-            "https://www.plantoeat.com/login",
+            baseUrl.format("login"),
             headers = {
                 'User-Agent': userAgent,
             }
         )
 
-        LOGGER.info(response.status_code)
+        if 200 != response.status_code:
+            LOGGER.error("Response from getting login page was {0}".format(response.status_code))
+            return
 
         soup = BeautifulSoup(response.text, "html.parser")
 
         csrf = soup.find_all("meta", attrs = {"name": "csrf-token"})
+
+        if not csrf or len(csrf) < 1:
+            LOGGER.error("Failed to extract CSRF token")
+            return
+
         data = {
             'authenticity_token': csrf[0]['content'],
             'login[email]': self.settings.get('username'),
@@ -65,16 +74,20 @@ class PlanToEat(MycroftSkill):
         }
 
         login_response = self.session.post(
-            "https://www.plantoeat.com/login",
+            baseUrl.format("login"),
             headers = {
                 'User-Agent': userAgent,
-                'Referer': 'https://www.plantoeat.com/login',
+                'Referer': baseUrl.format('login'),
                 'Accept': 'text/html,application/xhtml+xml',
                 'Accept-Language': 'en-US,en;q=0.5',
                 'Content-Type': 'application/x-www-form-urlencoded',
             },
             data = urlencode(data)
         )
+
+        if 200 != login_response.status_code:
+            LOGGER.error("Login request failed - status code: {0}".format(login_response.status_code))
+            return
 
         soup = BeautifulSoup(login_response.text, "html.parser")
         logout = soup.find_all('a', attrs = {"href": "/logout"})
@@ -83,16 +96,16 @@ class PlanToEat(MycroftSkill):
             LOGGER.error("Failed to login")
             return
 
-        LOGGER.info(login_response.status_code)
-
         shopping_lists = self.session.get(
-            "https://www.plantoeat.com/shopping_lists",
+            baseUrl.format("shopping_lists"),
             headers = {
                 'User-Agent': userAgent,
             }
         )
 
-        LOGGER.info(shopping_lists.status_code)
+        if 200 != shopping_lists.status_code:
+            LOGGER.error("Failed to fetch shopping lists page - status code: {0}".format(shopping_lists.status_code))
+            return
 
         soup = BeautifulSoup(shopping_lists.text, "html.parser")
         list_id_input = soup.find_all("input", id="shopping_list_id")
@@ -102,7 +115,6 @@ class PlanToEat(MycroftSkill):
             return
 
         self.shopping_list_id = list_id_input[0]['value']
-        LOGGER.info(self.shopping_list_id)
         self.logged_in = True
 
     @intent_file_handler('AddToList.intent')
@@ -115,19 +127,40 @@ class PlanToEat(MycroftSkill):
             self.speak_dialog('AddToList_failure', {'item': item_name})
 
     def _add_item_to_list(self, item_name):
-        requestData = submitString.format(self.shopping_list_id, quote(item_name))
+        category_suggestion = self._get_category_suggestion(item_name)
+
+        requestData = submitString.format(self.shopping_list_id, quote(item_name), category_suggestion)
 
         add_item_response = self.session.post(
-            "https://www.plantoeat.com/shopping_lists/update",
+            baseUrl.format("shopping_lists/update"),
             headers = {
                 'User-Agent': userAgent,
             },
             data = requestData
         )
-        LOGGER.info(requestData)
-        LOGGER.info(add_item_response.status_code)
+
+        if 200 != add_item_response.status_code:
+            LOGGER.error("Failed to add item to shopping list - status code: {0}".format(add_item_response.status_code))
+            return False
 
         return True
+
+    def _get_category_suggestion(self, item_name):
+        response = self.session.post(
+            baseUrl.format("shopping_lists/update"),
+            headers = {'User-Agent': userAgent},
+            data = urlencode({'title': item_name})
+        )
+
+        if response.status_code != 200:
+            return ""
+
+        json = response.json()
+
+        if json and len(json) > 0:
+            return json[0]
+        
+        return ''
     
 def create_skill():
     return PlanToEat()
