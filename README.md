27ce1f4148e984f616d5b750fead0582436a49c2
Peter Lind <peli@boozt.com>
2021-04-30 15:24:36 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,26 @@
+# <img src="https://raw.githack.com/FortAwesome/Font-Awesome/master/svgs/solid/clipboard-list.svg" card_color="#FD9E66" width="50" height="50" style="vertical-align:bottom"/> Plan to eat
+Manages a Plan to eat account
+
+## About
+This skill allows you to check the planned dishes on your Plan to eat account, as well as manage the shopping list.
+
+## Examples
+* "What's for dinner today?"
+* "Add milk to shopping list."
+* "Remove cheese from shopping list."
+* "What's on my shopping list?"
+
+## Todo
+So far, only adding items to the shopping list is implemented - left outstanding is still reporting from the shopping list, removing from the shopping list, and reporting from the planner
+
+## Credits
+fake51
+
+## Category
+**Productivity**
+
+## Tags
+#Shopping
+#List
+#Grocery
+#Plantoeat
---
+++ __init__.py
@@ -0,0 +1,206 @@
+from mycroft import MycroftSkill, intent_file_handler
+from bs4 import BeautifulSoup
+from requests import Session
+from urllib.parse import urlencode
+
+userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_9_3) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/35.0.1916.47 Safari/537.36'
+
+class PlanToEat(MycroftSkill):
+    def __init__(self):
+        MycroftSkill.__init__(self)
+        self.session = Session()
+        self.logged_in = False
+        self.shopping_list_id = None
+
+    def initialize(self):
+        response = self.session.get(
+            "https://www.plantoeat.com/login",
+            headers = {
+                'User-Agent': userAgent,
+            }
+        )
+
+        soup = BeautifulSoup(response.text, "html.parser")
+
+        csrf = soup.find_all("meta", attrs = {"name": "csrf-token"})
+        data = {
+            'authenticity_token': csrf[0]['content'],
+            'login[email]': self.settings.get('username'),
+            'login[password]': self.settings.get('password'),
+        }
+
+        login_response = self.session.post(
+            "https://www.plantoeat.com/login",
+            headers = {
+                'User-Agent': userAgent,
+                'Referer': 'https://www.plantoeat.com/login',
+                'Accept': 'text/html,application/xhtml+xml',
+                'Accept-Language': 'en-US,en;q=0.5',
+                'Content-Type': 'application/x-www-form-urlencoded',
+            },
+            data = urlencode(data)
+        )
+
+        shopping_lists = self.session.get(
+            "https://www.plantoeat.com/shopping_lists",
+            headers = {
+                'User-Agent': userAgent,
+            }
+        )
+
+        soup = BeautifulSoup(shopping_lists.text, "html.parser")
+        list_id_input = soup.find_all("input", id="shopping_list_id")
+        self.shopping_list_id = list_id_input[0]['value']
+
+        self.logged_in = True
+
+        return
+
+    @intent_file_handler('AddToList.intent')
+    def handle_add_to_list(self, message):
+        item_name = message.data.get('item')
+
+        if self._add_item_to_list(item_name):
+            self.speak_dialog('AddToList_success', {'item': item_name})
+        else:
+            self.speak_dialog('AddToList_failure', {'item': item_name})
+
+    def _add_item_to_list(self, item_name):
+        data = {
+            'shopping_list_id': self.shopping_list_id,
+            'autoStore': 1,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': item_name,
+            'ingredients[][note]': '',
+            'ingredients[][category]': 'Other',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+            'ingredients[][amount]': '',
+            'ingredients[][unit]': '',
+            'ingredients[][title]': '',
+            'ingredients[][note]': '',
+            'ingredients[][category]': '',
+            'ingredients[][store]': 0,
+        }
+
+        add_item_response = self.session.post(
+            "https://www.plantoeat.com/shopping_lists/update",
+            headers = {
+                'User-Agent': userAgent,
+            },
+            data = urlencode(data)
+        )
+
+        return True
+    
+def create_skill():
+    return PlanToEat()
+
